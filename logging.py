70c9d00642afc7ab43ff680373a06c3ff99c9efb
fredriksandberg <fredrik.sandberg@qls.se>
2018-01-18 01:31:11 +0100

    Moved log functions to it's own class

--- logging.py
+++ logging.py
@@ -1,5 +1,7 @@
 
 import json
+import datetime
+import time
 
 class logdata(object):
     ''' Check connection'''
@@ -23,3 +25,58 @@ class logdata(object):
             appendlog.writelines('\n')
 
         return
+
+    def set_startday(self):
+        global startDay
+        global maxPacketLost
+        global sumPacketLost
+
+        maxPacketLost = 0
+        sumPacketLost = 0
+        startDay = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        return
+
+    def get_startday(self):
+        global startDay
+
+        return startDay
+
+    def print_log(self, status, sleeptimer):
+
+        global sumPacketLost
+        global maxPacketLost
+
+        # Reset maxPacketLost on new day
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        if today != logdata.get_startday(self):
+            maxPacketLost = 0
+
+        print ('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('BSSID:', status['_BSSID'], '', 'LAST BSSID:', status['_LAST_BSSID']))
+        print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('SSID:', status['_SSID'], '', 'NOISE:', status['_NOISE']))
+        if status['_ROAM'] == 'TRUE':
+            print('{:<15} {:>20} {:>10} {:<15} {:>25} {:>3}'.format('RSSI:', status['_RSSI'], '', 'ROAM:\033[91m', status['_ROAM'], '\033[0m'))
+        else:
+            print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('RSSI:', status['_RSSI'], '', 'ROAM:', status['_ROAM']))
+        print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('LAST ROAM RSSI:', status['_LAST_ROAM'], '', 'TX SPEED:', status['_TRANSMITRATE']))
+        if status['_PACKETLOSS'] == 'TRUE':
+            status['_LOSS_COUNT'] = 1
+            sumPacketLost += 1
+            if sumPacketLost > maxPacketLost:
+                maxPacketLost = sumPacketLost
+            print('{:<15} {:>20} {:>10} {:<15} {:>23} {:>3}'.format('CHANNEL:', status['_CHANNEL'], '', 'PACKET LOST:\033[91m', status['_PACKETLOSS'], '\033[0m'))
+            print('{:<15} {:>20} {:>10} {:<15} {:>17}'.format('RTT:', status['_RTT'], '', 'MAX LOST LAST 24H:', maxPacketLost))
+        else:
+            status['_LOSS_COUNT'] = 0
+            sumPacketLost = 0
+            print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('CHANNEL:', status['_CHANNEL'], '', 'PACKET LOST:', status['_PACKETLOSS']))
+            print('{:<15} {:>20} {:>10} {:<15} {:>17}'.format('RTT:', status['_RTT'], '', 'MAX LOST LAST 24H:', maxPacketLost))
+        # No idea how to parse the return values :(
+        # if roamingLimit >= int(status['_RSSI']):
+        #    cachedResults = wifi.get_cachedscanresults()
+
+        print('------------------------------------------------------------------------------------')
+        time.sleep(sleeptimer)
+
+        return status
+
--- start.py
+++ start.py
@@ -1,24 +1,19 @@
 from osxwifi import wifistatus
 from connectivity import connectivity
-from logging import logdata
-import time
-import datetime
-
-
-
-
+from logging import *
 
 wifi = wifistatus()
 connection = connectivity()
 logdata = logdata()
 
+logToServer = True
+
+sleepTimer = 0.5  # Time in s between logging cycles
 remoteHost = '192.168.23.1'
 remotePort = 22
 timeout = 2  # Timeout in ms
 
 timeout = float(timeout)/1000
-sumPacketLost = 0
-maxPacketLost = 0
 
 clientMAC = wifi.get_hardwareaddress()
 roamingLimit = -10
@@ -28,46 +23,7 @@ status = {}
 status['_LAST_BSSID'] = ''
 status['_LAST_ROAM'] = '0'
 
-startDay = datetime.datetime.now().strftime('%Y-%m-%d')
-
-def printlog(statusdict, sleeptimer):
-    global sumPacketLost
-    global maxPacketLost
-
-    # Reset maxPacketLost on new day
-    runDay = datetime.datetime.now().strftime('%Y-%m-%d')
-    if runDay != startDay:
-        maxPacketLost = 0
-
-    print ('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('BSSID:', status['_BSSID'], '', 'LAST BSSID:', status['_LAST_BSSID']))
-    print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('SSID:', status['_SSID'], '', 'NOISE:', status['_NOISE']))
-    if status['_ROAM'] == 'TRUE':
-        print('{:<15} {:>20} {:>10} {:<15} {:>25} {:>3}'.format('RSSI:', status['_RSSI'], '', 'ROAM:\033[91m', status['_ROAM'], '\033[0m'))
-    else:
-        print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('RSSI:', status['_RSSI'], '', 'ROAM:', status['_ROAM']))
-    print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('LAST ROAM RSSI:', status['_LAST_ROAM'], '', 'TX SPEED:', status['_TRANSMITRATE']))
-    if status['_PACKETLOSS'] == 'TRUE':
-        status['_LOSS_COUNT'] = 1
-        sumPacketLost += 1
-        if sumPacketLost > maxPacketLost:
-            maxPacketLost = sumPacketLost
-        print('{:<15} {:>20} {:>10} {:<15} {:>23} {:>3}'.format('CHANNEL:', status['_CHANNEL'], '', 'PACKET LOST:\033[91m', status['_PACKETLOSS'], '\033[0m'))
-        print('{:<15} {:>20} {:>10} {:<15} {:>17}'.format('RTT:', status['_RTT'], '', 'MAX LOST LAST 24H:', maxPacketLost))
-    else:
-        status['_LOSS_COUNT'] = 0
-        sumPacketLost = 0
-        print('{:<15} {:>20} {:>10} {:<15} {:>20}'.format('CHANNEL:', status['_CHANNEL'], '', 'PACKET LOST:', status['_PACKETLOSS']))
-        print('{:<15} {:>20} {:>10} {:<15} {:>17}'.format('RTT:', status['_RTT'], '', 'MAX LOST LAST 24H:', maxPacketLost))
-    # No idea how to parse the return values :(
-    #if roamingLimit >= int(status['_RSSI']):
-    #    cachedResults = wifi.get_cachedscanresults()
-
-    print('------------------------------------------------------------------------------------')
-    time.sleep(sleeptimer)
-
-    return
-
-
+logdata.set_startday()
 
 while True:
     status['_BSSID'] = str(wifi.get_bssid())
@@ -97,7 +53,7 @@ while True:
         status['_LAST_BSSID'] = status['_BSSID']
         status['_LAST_ROAM'] = status['_RSSI']
 
-    printlog(status, 0.1)
-    logdata.write_log('logbuffer.tmp', status)
+    logdata.print_log(status, sleepTimer)
 
-    #sendlog('logbuffer.tmp')
+    if logToServer:
+        logdata.write_log('logbuffer.tmp', status)
